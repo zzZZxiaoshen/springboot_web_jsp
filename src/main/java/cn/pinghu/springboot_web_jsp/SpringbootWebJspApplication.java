e30fa61975b7fdc04a569315d1d06b8bb522de41
zzZZxiaoshen <644921352@qq.com>
2018-12-22 12:36:10 +0800

    commit

---
+++ src/main/java/cn/pinghu/springboot_web_jsp/SpringbootWebJspApplication.java
@@ -0,0 +1,14 @@
+package cn.pinghu.springboot_web_jsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebJspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebJspApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/cn/pinghu/springboot_web_jsp/SpringbootWebJspApplicationTests.java
@@ -0,0 +1,17 @@
+package cn.pinghu.springboot_web_jsp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootWebJspApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
