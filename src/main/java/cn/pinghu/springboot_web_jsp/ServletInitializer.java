d2378bc1c623b51fdb4f38df4be425cb0c1ced68
zzZZxiaoshen <644921352@qq.com>
2018-12-22 12:46:43 +0800

    commit

--- src/main/java/cn/pinghu/springboot_web_jsp/ServletInitializer.java
+++ src/main/java/cn/pinghu/springboot_web_jsp/ServletInitializer.java
@@ -1,14 +1,14 @@
 package cn.pinghu.springboot_web_jsp;
-
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SpringbootWebJspApplication.class);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringbootWebJspApplication.class);
+    }
 
 }
+
 
