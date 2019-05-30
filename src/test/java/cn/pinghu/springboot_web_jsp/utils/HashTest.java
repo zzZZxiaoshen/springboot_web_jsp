77e4497c2902ae21f847bc18d8bbe9270fdca8aa
沈凯 <shenkai@marsfood.cn>
2019-05-30 10:29:03 +0800

    commit

---
+++ src/test/java/cn/pinghu/springboot_web_jsp/utils/HashTest.java
@@ -0,0 +1,16 @@
+package cn.pinghu.springboot_web_jsp.utils;
+
+import org.junit.Test;
+
+public class HashTest {
+
+    @Test
+    public void test(){
+        Integer num = 45;
+        System.out.println(num.hashCode());
+        String st = "adadad";
+        System.out.println(st.hashCode());
+        Double d = 12.0;
+        System.out.println(d.hashCode());
+    }
+}
